d71b38b3c7fd925d5b31a4d70cfe2d5791928d4b
D-oro <107217252+D-oro@users.noreply.github.com>
2022-12-31 14:23:42 +0000

    first commit

---
+++ intro.ts
@@ -0,0 +1,26 @@
+let message = 'Hello world'
+
+console.log(message)
+
+//Types
+let anyType;
+anyType = 0;
+anyType = 3.5;
+anyType = 'bananas';
+anyType = true;
+anyType = null;
+anyType = undefined;
+
+//String
+let stringType = 'I am a string and cannot be reassigned to a different type';
+stringType = 'but I can become a different string';
+
+//Integer
+let integerType = 2;
+integerType = 4;
+
+//Boolean
+let booleanType = true;
+booleanType = false;
+
+// you get the point...
