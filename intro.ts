74b3eb3f3314288dbd6ac0e2b8cace9cffd6f447
D-oro <107217252+D-oro@users.noreply.github.com>
2022-12-31 15:24:11 +0000

    installed ts-node to run typescript in node.js

--- intro.ts
+++ intro.ts
@@ -1,5 +1,4 @@
 let message = 'Hello world'
-
 console.log(message)
 
 //Types
@@ -15,12 +14,38 @@ anyType = undefined;
 let stringType = 'I am a string and cannot be reassigned to a different type';
 stringType = 'but I can become a different string';
 
-//Integer
-let integerType = 2;
-integerType = 4;
+//Number
+let numberType = 2;
+numberType = 4.5;
 
 //Boolean
 let booleanType = true;
 booleanType = false;
 
-// you get the point...
+// you get the point... Typescript recognizes JS built in primitive data types
+// String, Number (not int, double etc.), Boolean, Null, Undefined
+
+//TypeAnnotations let us declare a type without declaring an initial value
+// we can use the types string, number, any
+let newString: string;
+newString = 'I am a string'
+
+let newBool: boolean;
+newBool = false;
+
+let newNumber: number;
+newNumber = 3;
+newNumber = 1.3;
+newNumber = 0;
+
+let newNull: null;
+newNull = null;
+
+let newUndefined: undefined;
+newUndefined= undefined;
+
+let newAny: any;
+newAny = 0;
+newAny = 'bananas';
+newAny = 3.5;
+newAny = true;
