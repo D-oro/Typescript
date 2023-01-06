99480a96b372cbd83a63652da2186050dfd24779
D-oro <107217252+D-oro@users.noreply.github.com>
2023-01-06 12:24:16 +0000

    default parameters in functions

--- functions.ts
+++ functions.ts
@@ -1,12 +1,12 @@
-// // function parameters can also be annotated!
+// function parameters can also be annotated!
 
 function greeting(name: string){
     return 'Hello ' + name
 }
 
-console.log(greeting('Doro'))
+console.log(greeting('Doro')) // prints Hello Doro
 
-// function parameters can stay optional with a ? after the parameter name
+// function parameters can stay optional with a ? after the parameter name. 
 // if we don't provide an alternative (here 'human'), then it prints 'Hello there undefined'
 
 function helloThere(name?: string){
@@ -14,3 +14,13 @@ function helloThere(name?: string){
 }
 
 helloThere() //prints Hello there human
+
+// we can also give the function parameter a default value 
+// that way we don't need to annotate them as the type is infered from the default value.
+
+function helloWorld(name = 'world'){
+    console.log(`Hello ${name}`)
+}
+
+helloWorld() // prints Hello world 
+helloWorld('Doro') // prints Hello Doro
